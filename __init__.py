d6d5e72097c7b1da0de7833780ac7d9db8c98d97
Gabriel Davini <gdavini@latinux.com.ar>
2015-06-23 13:46:30 -0300

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+
+import reporte_cc
+
+
+
+
---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+{
+    'name': 'reporte_cc',
+    'version': '1.01',
+    'depends': ["base","account"],
+    'author': 'Latinux S.A.',
+    'category': 'Own Modules',
+    'description': """ Crear reporte de Cuenta Corriente y volcarlo a un archivo .csv """,
+    'init_xml': [],
+    'update_xml': ['reporte_cc_view.xml'],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#   'certificate': 'certificate',
+} 
+
