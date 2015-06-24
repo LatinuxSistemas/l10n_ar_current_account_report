7c7535f52ca5597f9013afd4040d5b8b33e2a0ab
Gabriel Davini <gdavini@latinux.com.ar>
2015-06-24 16:19:52 -0300

    [ADD]
    Added filename handling.
    Added wizard donwload action.
    
    [MOD]
    
    Cleaned some lines.
    
    TODO: add translation files and play around with report file name.

--- wizard/current_account_report.py
+++ wizard/current_account_report.py
@@ -2,12 +2,10 @@
 
 import base64
 import codecs
-import csv
 import tempfile
 import time
 
-import netsvc
-from openerp.osv import fields, orm, osv
+from openerp.osv import fields, orm
 from openerp.tools.translate import _
 
 
@@ -16,9 +14,8 @@ class current_account_report(orm.TransientModel):
     _description = "Create Current Account Reports"
 
     _columns = {
+        'filename': fields.char('File Name', size=32),
         'data': fields.binary('Output file', readonly=True),
-        #'partner_ids': fields.many2many('res.partner', 'partner_wiz_cc_rel', 'wiz_id',
-        #                                'partner_id', 'Partners'),
         'partner_id': fields.many2one('res.partner', 'Partner', required=True),
         'state': fields.selection([('choose', 'Choose'), ('done', 'Done')], string="State"),
     }
@@ -50,11 +47,9 @@ class current_account_report(orm.TransientModel):
         if context is None:
             context = {}
 
-        logger = netsvc.Logger()
         sense = context.get("sense", "%")
         wizard = self.browse(cr, uid, ids)[0]
         invoice_obj = self.pool.get('account.invoice')
-        partner_obj = self.pool.get('res.partner')
 
         #sense means customer if out else supplier
         inv_ids = invoice_obj.search(cr, uid, [
@@ -62,40 +57,24 @@ class current_account_report(orm.TransientModel):
             ('type', 'like', "%s%%" % sense),
         ], order="date_invoice", context=context)
 
-        keys = ["date", "number", "desc", "source_doc", "amount_untaxed", "amount_total", "state"]
-        row_header = {
-            "date": _("Invoice Date"),
-            "state": _("Invoice Number"),
-            "desc": _("Description"),
-            "amount_untaxed": _("Amount untaxed ($)"),
-            "amount_total": _("Total ($)"),
-            "number": _("State"),
-            "source_doc": _("Source Document"),
-        }
-        row_header = ';'.join([row_header[l] for l in keys])+'\n'
-        rows = [row_header]
+        line_tmp = u'{};{};{};{};{};{};{}\n'
+        row_header = (_("Invoice Date"), _("Invoice Number"), _("Description"),
+                      _("Source Document"), _("Amount untaxed ($)"), _("Total ($)"), _("State"))
+        rows = [line_tmp.format(*row_header)]
 
         report_file = tempfile.mkstemp(suffix='.ccreport')[1]
-        with codecs.open(report_file, mode="wb", encoding="latin-1") as csvfile:
+        with codecs.open(report_file, mode="w", encoding="latin-1") as csvfile:
             header = _("Current Account Report for %s") % wizard.partner_id.name,
             today = time.strftime(_("%b %d, %Y")),
             csvfile.write("%s;%s\n\n" % (header[0], today[0]))
             invoices = invoice_obj.browse(cr, uid, inv_ids)
             for invoice in invoices:
-                # fecha factura
+                number = "%s %s" % (invoice.denomination_id.name or '', invoice.internal_number)
                 date = ''
                 if invoice.date_invoice:
                     date = time.strftime("%d/%m/%Y", time.strptime(invoice.date_invoice,
                                                                    '%Y-%m-%d'))
 
-                # posicion fiscal
-                pf = ''
-                if invoice.pos_ar_id:
-                    pf = invoice.pos_ar_id.name
-                elif invoice.denomination_id:
-                    pf = invoice.denomination_id.name
-
-                #state
                 if invoice['state'] == 'draft':
                     state = 'Borrador'
                 elif invoice['state'] == 'paid':
@@ -110,23 +89,36 @@ class current_account_report(orm.TransientModel):
                     state = ''
 
                 ##### LINEA DE REPORTE #####
-                row_data = {
-                    "date": date,
-                    "state": state,
-                    "number": pf + ' - ' + (invoice.number or ''),
-                    "amount_total": str(invoice.amount_total),
-                    "amount_untaxed": str(invoice.amount_untaxed),
-                    "desc": invoice.comment or invoice.name or '',
-                    "source_doc": invoice.origin or '',
-                }
-                row_data = ';'.join([row_data[l] for l in keys])+'\n'
-                print row_data
+                total = str(invoice.amount_total)
+                untaxed = str(invoice.amount_untaxed)
+                desc = invoice.comment or invoice.name or ''
+                origin = invoice.origin or '',
+                row_data = line_tmp.format(date, number, desc, origin, untaxed, total, state)
                 rows.append(row_data)
             csvfile.writelines(rows)
 
         f = open(report_file)
-        return self.write(cr, uid, ids, {
-            'state': 'done',
-            'data': f.read(),
-            'name': 'reporte_cuenta_corriente.csv'
-        }, context=context)
+        data = base64.encodestring(f.read())
+        f.close()
+
+        self.write(cr, uid, ids, {'state': 'done', 'data': data, 'filename': _('cc_report.csv')},
+                   context=context)
+
+        view_id = self.pool.get("ir.ui.view").search(cr, uid, [
+            ('name', '=', 'view_report_cc_wizard'),
+            ('model', '=', 'wizard.current.account.report'),
+            ('type', '=', 'form'),
+        ])
+        if view_id and isinstance(view_id, list):
+            view_id = view_id[0]
+
+        return {
+            'name': 'CC',
+            'res_model': 'wizard.current.account.report',
+            'type': 'ir.actions.act_window',
+            'views': [(view_id, 'form')],
+            'view_id': view_id,
+            'target': 'new',
+            'res_id': ids[0],
+            'context': context,
+        }
