60590119857d09a2042225bdee7802bc2465cf96
Gabriel Davini <gdavini@latinux.com.ar>
2015-06-24 17:37:15 -0300

    [MOD] Migrate to v8.0

--- wizard/current_account_report.py
+++ wizard/current_account_report.py
@@ -5,11 +5,11 @@ import codecs
 import tempfile
 import time
 
-from openerp.osv import fields, orm
+from openerp import models, api, fields
 from openerp.tools.translate import _
 
 
-class current_account_report(orm.TransientModel):
+class current_account_report(models.TransientModel):
     _name = "wizard.current.account.report"
     _description = "Create Current Account Reports"
 
@@ -24,11 +24,12 @@ class current_account_report(orm.TransientModel):
         'state': 'choose',
     }
 
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None,
-                        toolbar=False):
-        res = super(current_account_report, self).fields_view_get(cr, uid, view_id, view_type,
-                                                                  context, toolbar)
-        sense = context.get("sense", False)
+    @api.model
+    @api.noguess
+    def fields_view_get(self, view_id=False, view_type='form', toolbar=False, submenu=False):
+        res = super(current_account_report, self).fields_view_get(view_id, view_type,
+                                                                  toolbar=toolbar, submenu=submenu)
+        sense = self.env.context.get("sense", False)
         if sense:
             filter_dict = {
                 "out": [("customer", "=", True), _("Customer")],
@@ -37,25 +38,22 @@ class current_account_report(orm.TransientModel):
             domain = filter_dict[sense]
             string = domain.pop(1)
             partner_obj = self.pool["res.partner"]
-            partner_ids = partner_obj.search(cr, uid, domain)
-            partners = partner_obj.name_get(cr, uid, partner_ids)
+            partner_ids = partner_obj.search(domain)
+            partners = partner_obj.name_get(partner_ids)
             res["fields"]["partner_id"].update({"selection": partners, "string": string})
         return res
 
     #Facturas de Cliente
-    def create_report(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-
-        sense = context.get("sense", "%")
-        wizard = self.browse(cr, uid, ids)[0]
-        invoice_obj = self.pool.get('account.invoice')
+    def create_report(self):
+        print self
+        sense = self.env.context.get("sense", "%")
+        invoice_obj = self.env['account.invoice']
 
         #sense means customer if out else supplier
-        inv_ids = invoice_obj.search(cr, uid, [
-            ('partner_id', '=', wizard.partner_id.id),
+        inv_ids = invoice_obj.search([
+            ('partner_id', '=', self.partner_id.id),
             ('type', 'like', "%s%%" % sense),
-        ], order="date_invoice", context=context)
+        ], order="date_invoice")
 
         line_tmp = u'{};{};{};{};{};{};{}\n'
         row_header = (_("Invoice Date"), _("Invoice Number"), _("Description"),
@@ -64,10 +62,10 @@ class current_account_report(orm.TransientModel):
 
         report_file = tempfile.mkstemp(suffix='.ccreport')[1]
         with codecs.open(report_file, mode="w", encoding="latin-1") as csvfile:
-            header = _("Current Account Report for %s") % wizard.partner_id.name,
+            header = _("Current Account Report for %s") % self.partner_id.name,
             today = time.strftime(_("%b %d, %Y")),
             csvfile.write("%s;%s\n\n" % (header[0], today[0]))
-            invoices = invoice_obj.browse(cr, uid, inv_ids)
+            invoices = invoice_obj.browse(inv_ids)
             for invoice in invoices:
                 number = "%s %s" % (invoice.denomination_id.name or '', invoice.internal_number)
                 date = ''
@@ -101,24 +99,15 @@ class current_account_report(orm.TransientModel):
         data = base64.encodestring(f.read())
         f.close()
 
-        self.write(cr, uid, ids, {'state': 'done', 'data': data, 'filename': _('cc_report.csv')},
-                   context=context)
-
-        view_id = self.pool.get("ir.ui.view").search(cr, uid, [
-            ('name', '=', 'view_report_cc_wizard'),
-            ('model', '=', 'wizard.current.account.report'),
-            ('type', '=', 'form'),
-        ])
-        if view_id and isinstance(view_id, list):
-            view_id = view_id[0]
-
+        self.write({'state': 'done', 'data': data, 'filename': _('cc_report.csv')})
+        view = self.env.ref('view_report_cc_wizard')
         return {
             'name': 'CC',
             'res_model': 'wizard.current.account.report',
             'type': 'ir.actions.act_window',
-            'views': [(view_id, 'form')],
-            'view_id': view_id,
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
             'target': 'new',
-            'res_id': ids[0],
-            'context': context,
+            'res_id': self.ids[0],
+            'context': self.env.context,
         }
